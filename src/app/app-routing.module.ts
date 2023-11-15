4ac87289af07bd450eafd1aef6db25543cc9d624
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-11-15 09:44:16 +0700

    feat: import animation and optimized image module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {
+  RouterLink,
+  RouterModule,
+  RouterOutlet,
+  Routes,
+} from '@angular/router';
 import routes from '../configs/router/router.config';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes), RouterLink, RouterOutlet],
+  exports: [RouterModule, RouterLink, RouterOutlet],
 })
 export default class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import {
 
 import AppComponent from './app.component';
 import AppRoutingModule from './app-routing.module';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage,
+    BrowserAnimationsModule,
+  ],
+  providers: [provideHttpClient(withFetch()), provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export default class AppModule {}
