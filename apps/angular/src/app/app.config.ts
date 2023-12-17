e52099f1302b20b43c668aa0f2800c7941b43995
Hokki Suwanda <hokkyss2@gmail.com>
2023-12-17 19:03:18 +0700

    feat: add ngrx

--- apps/angular/src/app/app.config.ts
+++ apps/angular/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
+    provideStore(),
+    provideClientHydration(),
+    provideRouter(appRoutes),
+  ],
 };
