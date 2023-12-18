5c904018d63ae02aa7eeee29f5117f468388d1f2
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-12-18 14:22:17 +0700

    style: disable noPropertyAccessFromIndexSignature

--- apps/angular/server.ts
+++ apps/angular/server.ts
@@ -46,7 +46,7 @@ export function app(): express.Express {
   return server;
 }
 
-function run(): void {
+export function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
